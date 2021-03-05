d81daa9de2effa8b6afd549daf6aa4e4cbdbc65f
nico-php-coder <brjopr@mail.com>
2021-03-05 12:58:32 +0100

    continue styling...

--- View/home.view.php
+++ View/home.view.php
@@ -34,15 +34,16 @@
             <?php
                 for($i = 0; $i < 10; ++$i) {
                     $imgURL = '\'https://www.themoviedb.org/t/p/w600_and_h900_bestv2'.$movieList[$i]->m_image.'\'';
-                    $titleDiv = '<div class="title">'.$movieList[$i]->m_title.'</div>';
                     $genreString = '';
-
+                    
                     foreach($movieList[$i]->m_genreStrList as $value) {
                         $genreString .= $value.", ";
                     }
-                                        
+                    
+                    $titleDiv = '<div class="title"><b>#'.($i+1).' '.$movieList[$i]->m_title.'</b></div>';
                     $genreDiv = '<div class="genre">'.$genreString.'</div>';
-                    echo '<div class="top-10" style="background-image: url('.$imgURL.');">'.$titleDiv.'<br>'.$genreDiv.'</div>';
+                    $descriptionDiv = '<div class="description">'.$movieList[$i]->m_description.'</div>';
+                    echo '<div class="top-10" style="background-image: url('.$imgURL.');">'.$titleDiv.'<br>'.$genreDiv.'<br>'.$descriptionDiv.'</div>';
                 }
             ?>
         </section>
