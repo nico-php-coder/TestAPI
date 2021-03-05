005d992eaaa38d644455aa593a25d10fc034e7b7
nico-php-coder <brjopr@mail.com>
2021-03-05 11:08:30 +0100

    continuing style

--- View/home.view.php
+++ View/home.view.php
@@ -13,13 +13,28 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="home.view.css">
     <title>Top Movies</title>
 </head>
 <body>
-    <section class="top-movies">Top 10 section</section>
-    <section class="slider-movies">Slider</section>
+    <div class="main-view">
+        <section class="top-movies">Top 10 section
+            <div><?=$movieList[0]->m_title?></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </section>
+    
+        <section class="slider-movies">Slider
+    
+        </section>
+    </div>
 </body>
 </html>
