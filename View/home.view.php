6e606342a5accfd43d50689a7fd734896a3d824e
nico-php-coder <brjopr@mail.com>
2021-03-05 13:47:09 +0100

    sucefully implementing caroussel. continue to work on it. cleaning after.....

--- View/home.view.php
+++ View/home.view.php
@@ -14,6 +14,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="flickity.css">
     <link rel="stylesheet" href="home.view.css">
     <title>Top Movies</title>
 </head>
@@ -48,9 +49,34 @@
             ?>
         </section>
     
-        <section class="slider-movies">Slider
-    
+        <section class="slider-movies">
+
+            <!-- <p><code>wrapAround: true</code></p> -->
+        
+            <!-- Flickity HTML init -->
+            <div class="gallery js-flickity"
+            data-flickity-options='{ "wrapAround": true }'>
+
+                <?php
+                    for($i = 10; $i < 40; ++$i) {
+                        $imgURL = '\'https://www.themoviedb.org/t/p/w600_and_h900_bestv2'.$movieList[$i]->m_image.'\'';
+                        $genreString = '';
+                        
+                        foreach($movieList[$i]->m_genreStrList as $value) {
+                            $genreString .= $value.", ";
+                        }
+                        
+                        $titleDiv = '<div class="title"><b>#'.($i+1).' '.$movieList[$i]->m_title.'</b></div>';
+                        $genreDiv = '<div class="genre">'.$genreString.'</div>';
+                        $descriptionDiv = '<div class="description">'.$movieList[$i]->m_description.'</div>';
+                        echo '<div class="gallery-cell" style="background-image: url('.$imgURL.');">'.$titleDiv.'<br>'.$genreDiv.'<br>'.$descriptionDiv.'</div>';
+                    }
+                ?>
+
+            </div>
+
         </section>
     </div>
+    <script src="flickity.pkgd.min.js"></script>
 </body>
 </html>
---
+++ test.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="flickity.css">
+    <link rel="stylesheet" href="test.css">
+    <title>Document</title>
+</head>
+<body>
+    
+    <p><code>wrapAround: true</code></p>
+    
+    <!-- Flickity HTML init -->
+    <div class="gallery js-flickity"
+      data-flickity-options='{ "wrapAround": true }'>
+      <div class="gallery-cell"><img src="imgtest.jpg" height="100px"></div>
+      <div class="gallery-cell"></div>
+      <div class="gallery-cell"></div>
+      <div class="gallery-cell"></div>
+      <div class="gallery-cell"></div>
+    </div>
+
+    <script src="flickity.pkgd.min.js"></script>
+</body>    
+</html>
